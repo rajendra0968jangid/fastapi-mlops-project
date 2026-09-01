5ca0028d35dd6617cfc2fc6a042fa213b3c9b3e3
rajendra0968jangid <rajendra0968jangid@gmail.com>
2026-09-01 09:15:19 +0530

    code

--- backend/app.py
+++ backend/app.py
@@ -21,85 +21,33 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# MODEL_PATH = 'model (1).joblib' # model name
+MODEL_PATH = 'house_model.joblib' # model name
 
-# # Automatically train the model on startup if it doesn't exist yet
-# if not os.path.exists(MODEL_PATH):
-#     print("Model file not found. Triggering baseline training pipeline...")
-#     generate_and_train()
+# Automatically train the model on startup if it doesn't exist yet
+if not os.path.exists(MODEL_PATH):
+    print("Model file not found. Triggering baseline training pipeline...")
+    generate_and_train()
 
-# model = joblib.load(MODEL_PATH)
+model = joblib.load(MODEL_PATH)
 
-
-# ==========================
-# API Gateway URL
-# ==========================
-
-API_GATEWAY_URL = "https://a34n5jhwbh.execute-api.us-east-1.amazonaws.com/predict"
-
-# ==========================
-# Request Model
-# ==========================
-
-class Student(BaseModel):
-    maths: float
-    english: float
-
-
-@app.post("/predict")
-def predict(student: Student):
-
-    payload = student.model_dump()
-
-    try:
-
-        response = requests.post(
-            API_GATEWAY_URL,
-            json=payload,
-            headers={
-                "Content-Type": "application/json"
-            },
-            timeout=30
-        )
-
-        response.raise_for_status()
-
-        prediction = response.json()
-
-        return {
-            "status": "success",
-            "prediction": prediction
-        }
-
-    except requests.exceptions.HTTPError as e:
-        raise HTTPException(
-            status_code=response.status_code,
-            detail=response.text
-        )
-
-    except requests.exceptions.RequestException as e:
-        raise HTTPException(
-            status_code=500,
-            detail=str(e)
-        )
 
 # Define the incoming data schema using Pydantic
-# class HouseFeatures(BaseModel): # 
-#     maths: float
-#     english: float
-#     # num_rooms: float
+class HouseFeatures(BaseModel): # 
+    avg_income: float
+    house_age: float
+    num_rooms: float
     
-# @app.post("/predict")
-# def predict(features: HouseFeatures):
-#     try:
-#         # Perform inference using the validated schema fields
-#         prediction = model.predict([[features.maths, features.english]])
-#         return {
-#             "status": "success",
-#             "estimated_marks": round(float(prediction[0]), 2)
-#         }
-#     except Exception as e:
-#         raise HTTPException(status_code=400, detail=str(e))
+@app.post("/predict")
+def predict(features: HouseFeatures):
+    try:
+        # Perform inference using the validated schema fields
+        prediction = model.predict([[features.avg_income, features.house_age,features.num_rooms]])
+        return {
+            "status": "success",
+            "estimated_marks": round(float(prediction[0]), 2)
+        }
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
 if __name__ == '__main__':
     import uvicorn
